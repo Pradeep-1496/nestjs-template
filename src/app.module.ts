77f0e5638d50b212a702ccbb5d708ed92fa38fd2
pradeep-1496 <pph3400017@gmail.com>
2026-05-27 16:29:31 +0530

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,43 @@
 import { Module } from '@nestjs/common';
-import { LogsModule } from './modules/logs/logs.module';
 import { DatabaseModule } from './common/database/database.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     // DatabaseModule,
-    LogsModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'info',
+        transport: {
+          targets: [
+            {
+              target: 'pino/file',
+              level: 'info',
+              options: { destination: './logs/info.log', mkdir: true },
+            },
+            {
+              target: 'pino/file',
+              level: 'warn',
+              options: { destination: './logs/warn.log', mkdir: true },
+            },
+            {
+              target: 'pino/file',
+              level: 'error',
+              options: { destination: './logs/error.log', mkdir: true },
+            },
+            {
+              target: 'pino/file',
+              level: 'debug',
+              options: { destination: './logs/debug.log', mkdir: true },
+            },
+            {
+              target: 'pino-pretty',
+              options: { colorize: true },
+            },
+          ],
+        },
+      },
+    }),
   ],
 })
 export class AppModule {}
